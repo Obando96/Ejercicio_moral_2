b25b2d45fb606a6d676a6f91b94913603498bf08
Obando96 <joseobando30@gmail.com>
2023-01-30 12:56:11 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css'
+import Movies from './components/Movies';
+
+const APY_KEY = "ef13f188";
+let cartelera = [];
+function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchName, setSearchName] = useState("accion");
+
+  
+
+  const handleSearch = (e) => {
+    let errorString;
+    e.preventDefault();
+    if(!setSearchName(e.target.searchMovie.value)){
+      return (
+        errorString="Ingrse un nombre"
+      );
+    }
+    setSearchName(e.target.searchMovie.value);
+  }
+  const getMoviess = () =>{
+    const URL = `http://www.omdbapi.com/?apikey=${APY_KEY}&s=${searchName}`;
+    axios.get(URL)
+      .then((res) => setMovies(res.data.Search))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getMoviess();
+  }, [movies]);
+
+  return (
+    <div className="App">
+
+      <form className='formulario' onSubmit={handleSearch}>
+
+        <label htmlFor="">Buscar </label>
+        <input id='searchMovie' type="text" />
+
+        <button>Buscar</button>
+      </form>
+      
+      <section className='cartelera'>
+        {
+          movies.map(movie => {
+            return(
+              <Movies key={movie.imdbID}  Poster={movie.Poster} Title={movie.Title} Year={movie.Year}/>
+            );
+          })
+        }
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Movies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Movies = (props) => {
+  return (
+    <div className='container-movies'>
+        <img src={props.Poster} alt="" />
+        <h3 className='container-movies__title'>{props.Title}</h3>
+        <h3 className='container-movies__year'>{props.Year}</h3>
+    </div>
+    
+  )
+}
+
+export default Movies
